e0d24515e0665fa6234a5c01abbb3903ba8a775e
Emre Tosun <emre7osun@gmail.com>
2026-03-04 17:15:08 +0100

    "solved shortcut clash"

--- lua/emre/lazy/gitsigns.lua
+++ lua/emre/lazy/gitsigns.lua
@@ -3,6 +3,6 @@ return {
     config = function()
         require("gitsigns").setup()
 
-        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+        vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
     end,
 }
