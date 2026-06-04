147d7c2ea77a1605be09063460c3d7cb13fb9f88
emretosn <emre7osun@gmail.com>
2026-06-04 06:51:41 +0200

    cursor jumping after save fix

--- lua/emre/init.lua
+++ lua/emre/init.lua
@@ -28,7 +28,11 @@ autocmd('TextYankPost', {
 autocmd({"BufWritePre"}, {
     group = emreGroup,
     pattern = "*",
-    command = [[%s/\s\+$//e]],
+    callback = function()
+        local pos = vim.api.nvim_win_get_cursor(0)
+        vim.cmd([[%s/\s\+$//e]])
+        vim.api.nvim_win_set_cursor(0, pos)
+    end,
 })
 
 autocmd('LspAttach', {
