085126f48e3ed6f5c2a8fa4b29926d711f17bd8f
Emre Tosun <emre7osun@gmail.com>
2026-06-11 08:27:52 +0200

    treesitter fix

--- lua/emre/lazy/treesitter.lua
+++ lua/emre/lazy/treesitter.lua
@@ -1,12 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                "go", "lua", "python", "bash", "javascript", "typescript", "c", "cpp", "rust", "java", "yaml", "jsdoc", "vimdoc",
-            },
-            highlight = { enable = true },
-        })
-    end
+    opts = {
+        ensure_installed = {
+            "go", "lua", "python", "bash", "javascript", "typescript", "c", "cpp", "rust", "java", "yaml", "jsdoc", "vimdoc",
+        },
+        highlight = { enable = true },
+    }
 }
